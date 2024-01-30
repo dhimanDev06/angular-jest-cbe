a9b1936cff473383f37dbc4e6eb0d6c1256b2ab5
Bhattacharya <dhiman.bhattacharya@capgemini.com>
2024-01-30 10:57:08 +0530

    add login files

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -14,17 +14,36 @@ export class LoginComponent {
     email: "",
     password: ""
   };
-  doLogin(){
-    const isLogin = this.commnService.loginCheck(this.user);
+  formError: any = {
+    email: false,
+    password: false
+  };
+  isSubmit:boolean = false
+  
+  doLogin(data:any){
+    if(!data.value.email){
+      this.formError.email = true
+    }
+    if(!data.value.password){
+      this.formError.password = true
+    }
+    this.isSubmit = true
+    console.log("doLogin ----->", data.value,this.formError,this.user,this.isSubmit );
+    const isLogin = this.commnService.loginCheck(data.value);
     if(isLogin){
       this.router.navigateByUrl('/page')
     }
     console.log('user',this.user,isLogin)
   }
   reset(){
+    this.isSubmit = false
     this.user = {
       email: "",
       password: ""
     };
+    this.formError = {
+      email: false,
+      password: false
+    }
   }
 }
